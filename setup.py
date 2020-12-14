cef268da69f11e0e3b7b89261c1aa23fa74f2639
jarbasal <jarbasai@mailfence.com>
2020-12-14 07:29:15 +0000

    release 0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='shared_camera',
+    version='0.1',
+    packages=['shared_camera'],
+    url='https://github.com/NeonJarbas/shared_camera',
+    license='apache2',
+    author='jarbasAi',
+    author_email='jarbasai@mailfence.com',
+    description='inter process camera interface'
+)
